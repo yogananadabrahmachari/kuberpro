79b0e80f64e828f04f7122c8545d0520799be312
chari <brahmachary873@gmail.com>
2025-07-17 01:33:09 +0530

    Add db.js for MongoDB connection

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://brahmachary873:cjOQarNuIpQUkcEM@cluster0.hhzi5wt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Connected to Database");
+});
+
+module.exports = db;  // ✅ this line exports it, so require('../db') works
